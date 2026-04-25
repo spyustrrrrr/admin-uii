237cb7c0ae3acc8c012441d89aa64fe58f67cce4
spyustrrrrr <sulthan.yustr@gmail.com>
2026-04-25 13:19:13 +0700

    Understanding JSX in ReactJS and ES6(2)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState ([]);
+  //filter hanya kota non semarang yang di tampilkan
+  // const nonSemarangUsers = users.filter((user) => user.city !== "Semarang");
+	useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      console.error("[Component] Gagal menampilkan data:", error.message);
+    }
+  };
+  fetchData();
+}, []);
+
+
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
@@ -9,21 +26,9 @@ function Exercise() {
           User Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-          name="sulthan"
-          email="sulthan.yustr@gmail.com"
-          street="jl.sambiroto"
-          city="semarang"/>
-          <UserCard 
-          name="yustr"
-          email="yustr@gmail.com"
-          street="jl.sambiroto 3"
-          city="semarang"/>
-          <UserCard 
-          name="suwardhi"
-          email="suwardhi@gmail.com"
-          street="jl.sambiroto 4"
-          city="semarang"/>
+          {users.map((user) => (
+            <UserCard key={user.email} {...user} />
+          ))}
         </div>
       </div>
     </>
