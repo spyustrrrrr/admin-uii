c4951667342d22db23603b41513fec50fc851368
spyustrrrrr <sulthan.yustr@gmail.com>
2026-06-02 10:37:25 +0700

    Responsive Layout and React Icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import "./App.css";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
 import ErrorPage from "./pages/error";
+import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance"; 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function App() {
   const myRouter = createBrowserRouter([
-    {
+    // {
+    //   path: "/",
+    //   element: (
+    //     				<div className="flex justify-center items-center min-h-screen">
+    //       <Link to="/login" className="p-2 m-5 bg-primary text-white">
+    //         Login
+    //       </Link>
+    //       |
+    //       <Link to="/register" className="p-2 m-5 bg-primary text-white">
+    //         Register
+    //       </Link>
+    //     </div>
+    //   ),
+    //   errorElement: <ErrorPage />,
+    // },
+        {
       path: "/",
-      element: (
-        				<div className="flex justify-center items-center min-h-screen">
-          <Link to="/login" className="p-2 m-5 bg-primary text-white">
-            Login
-          </Link>
-          |
-          <Link to="/register" className="p-2 m-5 bg-primary text-white">
-            Register
-          </Link>
-        </div>
-      ),
+      element: <DashboardPage />,
       errorElement: <ErrorPage />,
     },
     {
@@ -29,6 +36,10 @@ function App() {
     {
       path: "/register",
       element: <SignUpPage />,
+    },
+    {
+      path: "/balance",
+      element: <BalancePage />,
     },
   ]);
 
