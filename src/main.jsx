e1907bba8f79182478e19957b1aaa4695a274d80
spyustrrrrr <sulthan.yustr@gmail.com>
2026-06-02 14:25:58 +0700

    State Management with Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-// import Exercise from './Exercise.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Parent from "./latihan/Parent.jsx";
+import { CounterContextProvider } from "./context/counterContext.jsx";
+import { ThemeContextProvider } from "./context/themeContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <CounterContextProvider>
+      <Parent />
+    </CounterContextProvider> */}
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </React.StrictMode>,
+);
