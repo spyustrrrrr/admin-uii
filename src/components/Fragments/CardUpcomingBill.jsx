767b0e01f30e0237133bc48897c4a1ad6213fef1
spyustrrrrr <sulthan.yustr@gmail.com>
2026-07-08 15:49:01 +0700

    Menambahkan integrasi backend bills dan expenses

--- src/components/Fragments/CardUpcomingBill.jsx
+++ src/components/Fragments/CardUpcomingBill.jsx
@@ -1,52 +1,79 @@
 import React from "react";
 import Card from "../Elements/Card";
+import Icon from "../Elements/Icon";
 import CircularProgress from "@mui/material/CircularProgress";
+
+const billIcons = {
+  "Figma - Yearly Plan": <Icon.Figma />,
+  "Adobe Inc - Yearly Plan": <Icon.Adobe />,
+};
 
 function CardUpcomingBill(props) {
   const { data = [] } = props;
   const isLoading = props.data == null;
 
-	return (
-    <>
-		<Card
-			title="Upcoming Bill"
-			link="/bill"
-	desc={
-		isLoading ? (
-			<div className="flex flex-col justify-center items-center h-full text-primary">
-				<CircularProgress color="inherit" size={50} enableTrackSlot />
-				Loading Data
-            </div>
+  return (
+    <Card
+      title="Upcoming Bill"
+      link="/bill"
+      desc={
+        isLoading ? (
+          <div className="flex flex-col justify-center items-center h-full text-primary">
+            <CircularProgress color="inherit" size={50} enableTrackSlot />
+            Loading Data
+          </div>
         ) : (
-        <div className="flex flex-col justify-around h-full">
-			{data.length === 0 && (
-            <div className="text-gray-03 text-sm">Tidak ada tagihan mendatang</div>
-        )}
-		{data.map((item) => (
-			<div key={item.id} className="flex justify-between pt-3 pb-3">
-				<div className="flex">
-				<div className="bg-special-bg p-4 rounded-lg flex flex-col">
-				<span className="text-xs">{item.month}</span>
-				<span className="text-2xl font-bold">{item.date}</span>
-			</div>
-			<div className="ms-10">
-				<span className="font-bold">{item.name}</span>
-				<br />
-				<span className="text-xs">Last Charge - {item.lastCharge}</span>
-				</div>
-			</div>
-			<div className="flex items-center">
-				<span className="py-2 px-4 border border-gray-05 rounded-lg font-bold">
-					${item.amount}
-				</span>
-			</div>
-			</div>
-			))}
-		</div>
+          <div className="flex flex-col justify-around h-full">
+            {data.length === 0 ? (
+              <div className="text-gray-03 text-sm">
+                Tidak ada tagihan mendatang
+              </div>
+            ) : (
+              data.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex justify-between items-center py-3"
+                >
+                  {/* Tanggal */}
+                  <div className="flex items-start">
+                    <div className="bg-special-bg p-4 rounded-lg flex flex-col items-center">
+                      <span className="text-xs">{item.month}</span>
+                      <span className="text-2xl font-bold">
+                        {item.date}
+                      </span>
+                    </div>
+
+                    {/* Icon + Detail Bill */}
+                    <div className="ms-6 flex flex-col">
+                      <div className="mb-1">
+                        {billIcons[item.name] || <Icon.Other />}
+                      </div>
+
+                      <div>
+                        <span className="font-bold">
+                          {item.name}
+                        </span>
+                        <br />
+                        <span className="text-xs">
+                          Last Charge - {item.lastCharge}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Harga */}
+                  <div className="flex items-center">
+                    <span className="py-2 px-4 border border-gray-05 rounded-lg font-bold">
+                      ${item.amount}
+                    </span>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         )
-        }
-      />
-    </>
+      }
+    />
   );
 }
 
